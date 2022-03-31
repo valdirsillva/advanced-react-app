62679d4c585940d9302d11351ffc4a7489f7272c
Valdir Silva <valdirpiresba@gmail.com>
2022-03-31 01:06:09 -0300

    conhecendo componentWillMount e componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Twitter from './Twitter'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    render() {
+
+        const posts = [
+            {
+                title: 'ReactJs',
+                description: 'Ciclo de vida do reactJs'
+            },
+            {
+                title: 'xpto',
+                description: 'xpto fluxo'
+            }
+        ]
+
+        return (
+            <Twitter  posts={posts}/>
+        )
+    }
 }
 
 export default App;
